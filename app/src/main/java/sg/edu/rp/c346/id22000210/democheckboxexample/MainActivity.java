50e5bdda49402fedf252160bce63dc82173ba2bc
Wayne <22000210@myrp.edu.sg>
2023-05-11 15:16:34 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22000210/democheckboxexample/MainActivity.java
@@ -0,0 +1,55 @@
+package sg.edu.rp.c346.id22000210.democheckboxexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    //Declaring the Variables
+    CheckBox cbEnabled;
+    Button btnCheck;
+    TextView tvShow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cbEnabled = findViewById(R.id.checkBoxDiscount);
+        btnCheck = findViewById(R.id.buttonCheck);
+        tvShow = findViewById(R.id.textView);
+
+
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("MyActivity", "Inside onClick()");
+                //Action to handle button click event
+                if (cbEnabled.isChecked()) {
+                    tvShow.setText("The discount is given");
+                }else {
+                    tvShow.setText("The discount is not given");
+                }
+            }
+        });
+
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Action for Toast message to show "Button Click"
+                Toast.makeText(MainActivity.this, "Button Click", Toast.LENGTH_LONG).show();
+                //context: The context to use, usually the application or the activity object
+                //text: the text to show in the Toast message
+                //duration: how long the display the message. Either LENGTH_LONG or LENGTH_SHORT
+            }
+        });
+
+
+    }
+}
